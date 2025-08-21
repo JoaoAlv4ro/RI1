b783a5e4845c6b6dd5f3c060851bdbef0136bbe1
João Álvaro <joaosilvaabc1@gmail.com>
2025-08-21 19:39:58 -0300

    Add thanos!

--- RI1.js
+++ RI1.js
@@ -37,49 +37,48 @@ let personagem6 = new Personagem("Homem Aranha", "Peter Parker", "Teia", 9, 8, 7
 let personagem7 = new Personagem("Pantera Negra", "T'Challa", "Garras", 8, 9, 8);
 let personagem8 = new Personagem("Doutor Estranho", "Strange", "Magia", 7, 8, 9);
 
+let thanos = new Personagem("Thanos", "Thanos?", "Manopla do Infinito", 7, 11, 11);
+
 let personagens = [personagem1, personagem2, personagem3, personagem4, personagem5, personagem6, personagem7, personagem8];
+
+console.log('Oponente!');
+console.log(thanos.descricao());
+console.log("-----------------------------");
+
 
 for (let i = 0; i < personagens.length; i++) {
     let personagem = personagens[i];
     console.log(personagem.descricao());
     console.log("-----------------------------");
-    for (let j = 0; j < personagens.length; j++) {
-        let oponente = personagens[j];
-        if (personagem === oponente) continue;
 
-        console.log('Oponente!');
-        console.log(oponente.descricao());
-        console.log("-----------------------------");
-        console.log("!Combate entre " + personagem.nome + " e " + oponente.nome + "!");
+    console.log("!Combate entre " + personagem.nome + " e " + thanos.nome + "!");
 
-        combateForca = personagem.getForca() - oponente.getForca();
-        combateVelocidade = personagem.getVelocidade() - oponente.getVelocidade();
-        combateResistencia = personagem.getResistencia() - oponente.getResistencia();
+    combateForca = personagem.getForca() - thanos.getForca();
+    combateVelocidade = personagem.getVelocidade() - thanos.getVelocidade();
+    combateResistencia = personagem.getResistencia() - thanos.getResistencia();
 
-        if (combateForca > 0) {
-            console.log(personagem.nome + " vence em força comparado ao " + oponente.nome);
-        } else if (combateForca < 0) {
-            console.log(oponente.nome + " vence em força comparado ao " + personagem.nome);
-        } else {
-            console.log(personagem.nome + " e " + oponente.nome + " empataram em força");
-        }
-
-        if (combateResistencia > 0) {
-            console.log(personagem.nome + " vence em resistência comparado ao " + oponente.nome);
-        } else if (combateResistencia < 0) {
-            console.log(oponente.nome + " vence em resistência comparado ao " + personagem.nome);
-        } else {
-            console.log(personagem.nome + " e " + oponente.nome + " empataram em resistência");
-        }
-
-        if (combateVelocidade > 0) {
-            console.log(personagem.nome + " vence em velocidade comparado ao " + oponente.nome);
-        } else if (combateVelocidade < 0) {
-            console.log(oponente.nome + " vence em velocidade comparado ao " + personagem.nome);
-        } else {
-            console.log(personagem.nome + " e " + oponente.nome + " empataram em velocidade");
-        }
-        console.log("-----------------------------");
-
+    if (combateForca > 0) {
+        console.log(personagem.nome + " vence em força comparado ao " + thanos.nome);
+    } else if (combateForca < 0) {
+        console.log(thanos.nome + " vence em força comparado ao " + personagem.nome);
+    } else {
+        console.log(personagem.nome + " e " + thanos.nome + " empataram em força");
     }
+
+    if (combateResistencia > 0) {
+        console.log(personagem.nome + " vence em resistência comparado ao " + thanos.nome);
+    } else if (combateResistencia < 0) {
+        console.log(thanos.nome + " vence em resistência comparado ao " + personagem.nome);
+    } else {
+        console.log(personagem.nome + " e " + thanos.nome + " empataram em resistência");
+    }
+
+    if (combateVelocidade > 0) {
+        console.log(personagem.nome + " vence em velocidade comparado ao " + thanos.nome);
+    } else if (combateVelocidade < 0) {
+        console.log(thanos.nome + " vence em velocidade comparado ao " + personagem.nome);
+    } else {
+        console.log(personagem.nome + " e " + thanos.nome + " empataram em velocidade");
+    }
+    console.log("-----------------------------");
 }
